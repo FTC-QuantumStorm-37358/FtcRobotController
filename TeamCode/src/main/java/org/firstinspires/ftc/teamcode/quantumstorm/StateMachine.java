6732ca9171527a8c12c50b77a245d284e1f321d0
joeliao426 <fangyunshen88@gmail.com>
2026-09-23 19:29:49 -0700

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/quantumstorm/StateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/quantumstorm/StateMachine.java
@@ -1,4 +1,57 @@
 package org.firstinspires.ftc.teamcode.quantumstorm;
 
-public class StateMachine {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class StateMachine extends LinearOpMode {
+
+    enum State {
+        INIT,
+        MOVING,
+        READY_TO_SHOOT,
+        READY_TO_PICKUP,
+        ERROR,
+        PARK,
+    }
+
+    State state = State.INIT;
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        while (opModeIsActive()) {
+            switch (state) {
+                case INIT:
+                    //switch states when something is true
+
+                        state = State.MOVING;
+
+                    break;
+
+                case MOVING:
+                    //switch states when something is true
+
+                        state = State.READY_TO_SHOOT;
+
+                    break;
+
+                case READY_TO_SHOOT:
+                    //switch states when something is true
+
+                        state = State.READY_TO_PICKUP;
+
+                    break;
+
+                case READY_TO_PICKUP:
+                    break;
+
+                case ERROR:
+                    break;
+
+                case PARK:
+                    break;
+            }
+        }
+    }
 }
